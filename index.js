2e752a62840ab233d3acbe88cee8c6e107524be8
masud2005 <masud20012005@gmail.com>
2025-01-15 01:01:05 +0600

    Admin Role Updated and Delete APIs Added

--- index.js
+++ index.js
@@ -84,13 +84,34 @@ async function run() {
 
             // insert email if user doesn't exists: 
             // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
-            const query = {email: user.email}
+            const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query)
-            if(existingUser){
-                return res.send({message: 'User already exists', insertedId: null})
+            if (existingUser) {
+                return res.send({ message: 'User already exists', insertedId: null })
             }
 
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        // Update Role
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        // Delete 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await usersCollection.deleteOne(query);
             res.send(result);
         })
 
